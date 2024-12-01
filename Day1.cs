6ffdaca02c3c965820d8146c054af017ad51f917
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-01 11:49:32 +0100

    Day1, Part 1

--- Day1.cs
+++ Day1.cs
@@ -7,6 +7,23 @@ class Day1 {
 
         var lines = Helper.getInputAsLines(1);
 
-        Console.WriteLine($"result: {null}");
+        List<long> list1 = new();
+        List<long> list2 = new();
+
+        foreach(string l in lines){
+            var s = l.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            list1.Add(long.Parse(s[0]));
+            list2.Add(long.Parse(s[1]));
+        }
+
+        list1.Sort();
+        list2.Sort();
+
+        long sumA=0;
+        for (int i = 0; i < list1.Count; i++){
+            sumA+=Math.Abs(list2[i] - list1[i]);
+        }
+
+        Console.WriteLine($"result: {sumA}");
     }
 }
