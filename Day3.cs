fbd93de02d2fc671a3cdbf0896ef7149dabdf7d6
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-03 07:03:56 +0100

    D3P2

--- Day3.cs
+++ Day3.cs
@@ -11,9 +11,17 @@ class Day3 {
         long sumB=0;
 
         System.Text.RegularExpressions.Regex regex = new Regex(@"mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)");
-        SortedDictionary<int,int> muls = new();
         regex.Matches(lines).ToList().ForEach(match => {
             sumA+=int.Parse(match.Groups["a"].Value)*int.Parse(match.Groups["b"].Value);
+        });
+
+        System.Text.RegularExpressions.Regex dodont = new Regex(@"(?:(?:don't|do)\(\)|^).+?(?=(?:don't\(\)|do\(\)|$))", RegexOptions.Singleline);
+        dodont.Matches(lines).ToList().ForEach(match => {
+            if (!match.Value.StartsWith("don't")) {
+                regex.Matches(match.Value).ToList().ForEach(match2 => {
+                    sumB+=int.Parse(match2.Groups["a"].Value)*int.Parse(match2.Groups["b"].Value);
+                });
+            }
         });
 
         Console.ForegroundColor=ConsoleColor.Blue;
