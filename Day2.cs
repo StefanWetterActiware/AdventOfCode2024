7dce9f03f3979866f2145b99cc64b19b4212050d
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-03 06:38:23 +0100

    D2P2 - incorrect...

--- Day2.cs
+++ Day2.cs
@@ -3,21 +3,41 @@ using System.Linq;
 
 
 class Day2 {
-    static bool doLine(List<int> intParts) {
+    static bool doLine(List<int> intParts, int allowedErrors, out int removedIndex) {
+        removedIndex = -1;
+
         var compare = intParts[0]>intParts[1] ? -1 : 1;
+        var didTestWithoutFirst = false;
         for (int j=0; j<intParts.Count-1; j++){
             var diff = intParts[j+1] - intParts[j];
-            if (Math.Abs(diff) >3){return false;}
-            if (Math.Sign(diff) != Math.Sign(compare)) return false;
+            if (Math.Abs(diff) >3 || Math.Sign(diff) != Math.Sign(compare)) {
+                if (allowedErrors == 0) return false;
+
+                if (!didTestWithoutFirst) {
+                    var test2 = intParts.ToList();
+                    test2.RemoveAt(0);
+                    var withoutFirst = doLine(test2,(allowedErrors-1), out int dummy);
+                    if (withoutFirst) {
+                        removedIndex=0;
+                        return true;
+                    }
+                    didTestWithoutFirst=true;
+                }
+                var test = intParts.ToList();
+                test.RemoveAt(j+1);
+                removedIndex=j+1;
+                return doLine(test,(allowedErrors-1), out int dummy2);
+            }
         }
         return true;
     }
 
     internal static void doit(){
         Regex dayNoR = new(@"\d*$");
-        var lines = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value));
+        var lines = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value), true);
 
         long sumA=0;
+        long sumB=0;
 
         foreach (var line in lines){
             var parts=line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
@@ -25,13 +45,30 @@ class Day2 {
             List<int> intParts = new();
             parts.ToList().ForEach( a=> intParts.Add(int.Parse(a)));
 
-            if (doLine(intParts)) {
+            if (doLine(intParts,0, out int dummy)) {
                 sumA++;
+            }
+            if (doLine(intParts,1, out int removedIndex)) {
+                sumB++;
+                if (removedIndex > -1) {
+                    Console.ForegroundColor=ConsoleColor.Green;
+                    Console.Write(string.Join(' ', intParts.Take(removedIndex+1).ToArray()));
+                    Console.ForegroundColor=ConsoleColor.Red;
+                    Console.Write($" {intParts[removedIndex]} ");
+                    Console.ForegroundColor=ConsoleColor.Green;
+                    Console.WriteLine(string.Join(' ', intParts.Skip(removedIndex+2).ToArray()));
+                } else {
+                    Console.ForegroundColor=ConsoleColor.Green;
+                    Console.WriteLine(string.Join(' ', intParts.ToArray()));
+                }
+            } else {
+                Console.ForegroundColor=ConsoleColor.Red;
+                Console.WriteLine(string.Join(' ', intParts.ToArray()));
             }
         }
 
-
+        Console.ForegroundColor=ConsoleColor.Blue;
         Console.WriteLine($"result: {sumA}");
-        //Console.WriteLine($"result B: {sumB}");
+        Console.WriteLine($"result B: {sumB}");
     }
 }
