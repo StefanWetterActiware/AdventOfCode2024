7a9d0838d11cdd5405a6dea8bc71fe0a2ac5c9eb
Stefan Wetter <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-09 13:48:13 +0100

    D8P2

--- Day8.cs
+++ Day8.cs
@@ -1,14 +1,12 @@
 using System.Drawing;
+using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 class Day8 {
 
     internal static void doit(){
         Regex dayNoR = new(@"\d*$");
-        var lines = Helper.getInputAsCharArray(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType!.Name).Value));
-        
-        long sumA=0;
-        long sumB=0;
+        var lines = Helper.getInputAsCharArray(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType!.Name).Value), false);
 
         SortedDictionary<char, List<Point>> allAntennas = new();
         for (int y = 0; y < lines.Length; y++)
@@ -25,6 +23,7 @@ class Day8 {
         }
 
         List<Point> allAnti = new();
+        List<Point> allAntiB = new();
 
         foreach (var freqKVP in allAntennas) {
             var freq=freqKVP.Key;
@@ -34,17 +33,40 @@ class Day8 {
                 foreach (var ant2 in antennas) {
                     if (!ant1.Equals(ant2)) {
                         var newAnti = new Point(ant2.X + (ant2.X-ant1.X), ant2.Y + (ant2.Y-ant1.Y));
-                        if (newAnti.X >= 0 && newAnti.Y >= 0 && newAnti.X < lines[0].Length && newAnti.Y < lines.Length && !allAnti.Contains(newAnti)) {
+                        if (Helper.isOnGrid(lines, newAnti.X, newAnti.Y) && !allAnti.Contains(newAnti)) {
                             allAnti.Add(newAnti);
                         }
+
+                        do {
+                            if (Helper.isOnGrid(lines, newAnti.X, newAnti.Y)) {
+                                if (!allAntiB.Contains(newAnti)) allAntiB.Add(newAnti);
+                                newAnti = new Point(newAnti.X + (ant2.X-ant1.X), newAnti.Y + (ant2.Y-ant1.Y));
+                            } else
+                                break;
+                        } while (true);
+
+                        newAnti = new Point(ant1.X - (ant2.X-ant1.X), ant1.Y - (ant2.Y-ant1.Y));
+                        do {
+                            if (Helper.isOnGrid(lines, newAnti.X, newAnti.Y)) {
+                                if (!allAntiB.Contains(newAnti)) allAntiB.Add(newAnti);
+                                newAnti = new Point(newAnti.X - (ant2.X-ant1.X), newAnti.Y - (ant2.Y-ant1.Y));
+                            } else
+                                break;
+                        } while (true);
                     }
                 }
             }
         }
 
+        foreach (var item in allAntennas.Where(x => x.Value.Count>1)) {
+            item.Value.ForEach(x => {if (!allAntiB.Contains(x)) allAntiB.Add(x);});
+        }
+        
+
 
         Console.ForegroundColor=ConsoleColor.Blue;
         Console.WriteLine($"result: {allAnti.Count}");
-        Console.WriteLine($"result B: {sumB}");
+        Console.WriteLine($"result B: {allAntiB.Count}");
+
     }
 }
