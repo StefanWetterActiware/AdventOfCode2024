f1a000045db7e6227b369457860182186317b9f2
Stefan Wetter <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-12 09:07:53 +0100

    Tag per Parameter

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿
-//Day17.doit();return;
-
 var lastDayT = System.Reflection.Assembly.GetExecutingAssembly().GetTypes().Where(a => a.Name.StartsWith("Day")).OrderByDescending(a => int.Parse(a.Name.Replace("Day",""))).First();
-//var lastDay = System.Activator.CreateInstance(lastDayT);
+
+if (Environment.GetCommandLineArgs().Any() && Environment.GetCommandLineArgs().Any(x => x.StartsWith("Day"))) {
+	try {
+		lastDayT = System.Reflection.Assembly.GetExecutingAssembly().GetTypes().Single(a => a.Name.Equals(Environment.GetCommandLineArgs().First(x => x.StartsWith("Day"))));
+		Console.WriteLine("Loaded Class from CmdLineArgs");
+	} catch {}
+}
 
 var doitM = lastDayT.GetMethod("doit", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
 doitM.Invoke(null,null);
