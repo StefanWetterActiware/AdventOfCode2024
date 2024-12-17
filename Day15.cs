13a05bdb86448c6595f40f0b9b51f66f00e5a7e8
Stefan Wetter <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-17 20:37:15 +0100

    D15P2

--- Day15.cs
+++ Day15.cs
@@ -4,20 +4,40 @@ using Microsoft.VisualBasic;
 
 class Day15 {
     
-    internal static bool move (ref char[][] grid, int X, int Y, int mx, int my) {
+    internal static bool canmove (ref char[][] grid, int X, int Y, int mx, int my) {
         char next = grid[Y + my][X + mx];
         if (next == '#') return false;
-        if (next == 'O') {
-            var canMove = move(ref grid, X + mx, Y + my, mx, my);
-            if (canMove) {
-                grid[Y + my][X + mx] = 'O';
-                return true;
+        if (next == '.') return true;
+        if ((grid[Y + my][X + mx] == 'O') || ((grid[Y + my][X + mx] == '[' || grid[Y + my][X + mx] == ']') && my == 0)) {
+            return canmove(ref grid, X + mx, Y + my, mx, my);
+        } else if (grid[Y + my][X + mx] == '[' || grid[Y + my][X + mx] == ']') {
+            if (grid[Y + my][X + mx] == '[') {
+                return canmove(ref grid, X + mx, Y + my, mx, my) && canmove(ref grid, X + mx + 1, Y + my, mx, my);
             } else {
-                return false;
+                return canmove(ref grid, X + mx, Y + my, mx, my) && canmove(ref grid, X + mx - 1, Y + my, mx, my);
+            }
+        } else
+            return true;
+    }
+
+    internal static void move(ref char[][] grid, int X, int Y, int mx, int my) {
+        char next = grid[Y + my][X + mx];
+        if (next == '#')
+            return;
+
+        if ((grid[Y + my][X + mx] == 'O') || ((grid[Y + my][X + mx] == '[' || grid[Y + my][X + mx] == ']') && my == 0)) {
+            move(ref grid, X + mx, Y + my, mx, my);
+        } else if (grid[Y + my][X + mx] == '[' || grid[Y + my][X + mx] == ']') {
+            if (grid[Y + my][X + mx] == '[') {
+                move(ref grid, X + mx, Y + my, mx, my);
+                move(ref grid, X + mx + 1, Y + my, mx, my);
+            } else {
+                move(ref grid, X + mx, Y + my, mx, my);
+                move(ref grid, X + mx - 1, Y + my, mx, my);
             }
         }
-        grid[Y + my][X + mx] = 'O';
-        return true;
+        grid[Y + my][X + mx] = grid[Y][X];
+        grid[Y][X] = '.';
     }
 
     internal static void doit(){
@@ -71,21 +91,15 @@ class Day15 {
         foreach (char m in moves) {
             int mx= m=='>'?1:m=='<'?-1:0;
             int my = m=='^'?-1:m=='v'?1:0;
-            if (grid[rob.Y + my][rob.X + mx] == '#') {
-                continue;
-            } else if (grid[rob.Y + my][rob.X + mx] == 'O') {
-                if (move(ref grid, rob.X + mx, rob.Y + my, mx, my)) {
-                    grid[rob.Y][rob.X] = '.';
-                    rob = new Point(rob.X + mx, rob.Y + my);
-                    grid[rob.Y][rob.X] = '@';
-                }
-            } else {
+            if (canmove(ref grid, rob.X, rob.Y, mx, my)) {
+                move(ref grid, rob.X, rob.Y, mx, my);
                 rob = new Point(rob.X + mx, rob.Y + my);
+                grid[rob.Y][rob.X] = '@';
             }
         }
 
         for (int y = 0; y < grid.Length; y++) {
-            if (grid[y].Contains('@')) {
+            if (grid[y].Contains('O')) {
                 for (int x = 0; x < grid[0].Length; x++) {
                     if (grid[y][x] == 'O') {
                         sumA += 100*y+x;
@@ -94,8 +108,54 @@ class Day15 {
             }
         }
 
-
         Console.WriteLine($"result A: {sumA}");
+
+        var printGrid = () => {
+            Console.Clear();
+            for (int y = 0; y < grid.Length; y++) {
+               Console.WriteLine(string.Join("",grid[y]));
+            }
+            Thread.Sleep(100);
+        };
+
+        //reset grid & robbie
+        grid = input.First().Select(x => x.Replace("#", "##").Replace(".", "..").Replace("O", "[]").Replace("@", "@.").ToCharArray()).ToArray();
+        for (int y = 0; y < grid.Length; y++) {
+            if (grid[y].Contains('@')) {
+                for (int x = 0; x < grid[0].Length; x++) {
+                    if (grid[y][x] == '@') {
+                        rob = new Point(x, y);
+                        break;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        //run again
+        foreach (char m in moves) {
+            int mx= m=='>'?1:m=='<'?-1:0;
+            int my = m=='^'?-1:m=='v'?1:0;
+            if (canmove(ref grid, rob.X, rob.Y, mx, my)) {
+                move(ref grid, rob.X, rob.Y, mx, my);
+                rob = new Point(rob.X + mx, rob.Y + my);
+                grid[rob.Y][rob.X] = '@';
+            }
+            //printGrid();
+        }
+
+        for (int y = 0; y < grid.Length; y++) {
+            if (grid[y].Contains('[')) {
+                for (int x = 0; x < grid[0].Length; x++) {
+                    if (grid[y][x] == '[') {
+                        sumB += 100 * y + x;
+                    }
+                }
+            }
+        }
+
+
         Console.WriteLine($"result B: {sumB}");
     }
 }
