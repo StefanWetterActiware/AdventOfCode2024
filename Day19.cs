a188107a955f104ca37e15345acc270f23cd0b4e
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-19 18:04:21 +0100

    D19P2

--- Day19.cs
+++ Day19.cs
@@ -9,11 +9,21 @@ class Day19 {
         Regex dayNoR = new(@"\d*$");
         var input = Helper.getBlocks(Helper.getInput(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType!.Name).Value)));
 
+        // input = Helper.getBlocks("""
+        //                          r, wr, b, g, bwu, rb, gb, br
+        //                          
+        //                          brwrr
+        //                          bggr
+        //                          gbbr
+        //                          rrbgbr
+        //                          ubwu
+        //                          bwurrg
+        //                          brgr
+        //                          bbrgwb
+        //                          """);
+
         var towels = input.First().Single().Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
         var patterns = input.Skip(1).Take(1).Single();
-
-        //         input = Helper.getBlocks("""
-        //                                  """);
 
         long sumA=0;
         long sumB=0;
@@ -22,33 +32,46 @@ class Day19 {
         // So, let's go recursive with cache...
 
         foreach (var pattern in patterns) {
-            Console.WriteLine(pattern);
+            Console.Write(pattern + ": ");
 
-            if (canBuildFromPattern(pattern, towels))
-                sumA++;
+            countPatternPossibilities(pattern, towels, out long count);
+            sumB += count;
+            if (count > 0) sumA++;
+            
+            Console.WriteLine($"{count} possibilities");
         }
 
         Console.WriteLine($"result A: {sumA}");
         Console.WriteLine($"result B: {sumB}");
     }
 
-    static List<string> notWorking=new();
+    static SortedDictionary<string, long> countDict = new();
 
-    static bool canBuildFromPattern(string input, List<string> towels) {
-        if (string.IsNullOrEmpty(input)) return true;
-        if (towels.Contains(input)) return true;
-        if (notWorking.Contains(input)) return false;
-        if (input.Length == 1) return false;
-
-        foreach (var towel in towels) {
-            var i = input.IndexOf(towel);
-            if (i == -1) continue;
-
-            string next1 = input.Substring(0,i);
-            string next2 = input.Substring(i + towel.Length);
-            if (canBuildFromPattern(next1, towels) && canBuildFromPattern(next2, towels)) return true;
+    static void countPatternPossibilities(string input, List<string> towels, out long count) {
+        if (countDict.ContainsKey(input)) {
+            count = countDict[input];
+            return;
         }
-        notWorking.Add(input);
-        return false;
+        countPatternPossibilitiesDirect(input, towels, out long c);
+        countDict.Add(input, c);
+        count = c;
+    }
+
+    static void countPatternPossibilitiesDirect(string input, List<string>towels, out long count) {
+        count = 0;
+        
+        if (string.IsNullOrWhiteSpace(input)) {
+            count = 1;
+            return;
+        }
+        
+        foreach (var towel in towels) {
+            if (!input.StartsWith(towel))
+                continue;
+
+            string next = input.Substring(towel.Length);
+            countPatternPossibilities(next, towels, out long countA);
+            count += countA;
+        }
     }
 }
