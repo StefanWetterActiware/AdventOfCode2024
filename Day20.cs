4f236e5c571b4381fdd00ed25eb9c4d51c5f0a4e
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-20 16:24:41 +0100

    D20P1

--- Day20.cs
+++ Day20.cs
@@ -22,7 +22,7 @@ class Day20 {
         for (int y = 0; y < input.Length; y++) {
             for (int x = 0; x < input[0].Length; x++) {
                 if (input[y][x] == 'S') start = new Point(x, y);
-                if (input[y][x] == 'E') start = new Point(x, y);
+                if (input[y][x] == 'E') end = new Point(x, y);
             }
         }
 
@@ -32,17 +32,33 @@ class Day20 {
         toCalculate.Enqueue(new RacePoint{cheats=0, X = start.X, Y = start.Y});
 
         while (toCalculate.TryDequeue(out var rP)) {
+            if (!Helper.isOnGrid(input, rP.X, rP.Y)) continue;
+            if (input[rP.Y][rP.X] == '#') continue;
+            
             if (rP.X == end.X && rP.Y == end.Y) {
                 calculatedRacePoints.Add(rP);
                 continue;
             }
 
-            if (calculatedRacePoints.Any(x => x.X == rP.X && x.Y == rP.Y && x.cheats == rP.cheats && x.currentScore <= rP.cheats)) {
+            if (calculatedRacePoints.Any(x => x.X == rP.X && x.Y == rP.Y && x.cheats == rP.cheats && x.currentScore <= rP.currentScore)) {
                 continue;
             }
 
             calculatedRacePoints.RemoveAll(x => x.X == rP.X && x.Y == rP.Y && x.cheats == rP.cheats);
             calculatedRacePoints.Add(rP);
+            
+            toCalculate.Enqueue(new RacePoint{cheats=0, X = rP.X+1, Y = rP.Y, currentScore = rP.currentScore+1, previous = rP});
+            toCalculate.Enqueue(new RacePoint{cheats=0, X = rP.X-1, Y = rP.Y, currentScore = rP.currentScore+1, previous = rP});
+            toCalculate.Enqueue(new RacePoint{cheats=0, X = rP.X, Y = rP.Y+1, currentScore = rP.currentScore+1, previous = rP});
+            toCalculate.Enqueue(new RacePoint{cheats=0, X = rP.X, Y = rP.Y-1, currentScore = rP.currentScore+1, previous = rP});
+        }
+
+        var shortest = calculatedRacePoints.Single(x => x.X == end.X && x.Y == end.Y).currentScore;
+        Console.WriteLine($"result: {shortest}");
+
+        for (int i = 0; i < calculatedRacePoints.Count; i++) {
+            var pA = calculatedRacePoints[i];
+            sumA += calculatedRacePoints.Skip(i+1).Count(x => Math.Abs(x.X - pA.X) + Math.Abs(x.Y-pA.Y) == 2 && Math.Abs(pA.currentScore-x.currentScore) > 101);
         }
 
         Console.ForegroundColor=ConsoleColor.Blue;
