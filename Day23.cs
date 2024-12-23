8df299ae7819aae81fd1a8b3fec65922d74c3740
Stefan Wetter <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-23 09:14:16 +0100

    D23P2

--- Day23.cs
+++ Day23.cs
@@ -1,4 +1,3 @@
-using System.Drawing;
 using System.Text.RegularExpressions;
 
 class Day23 {
@@ -8,12 +7,12 @@ class Day23 {
         // input = """
         //         """.Split('\n');
         long sumA=0;
-        long sumB=0;
-
-        List<string> sets = new();
 
         for (int i = 0; i < input.Count(); i++) {
             var comps = input[i].Split('-');
+
+            recurse($"{comps[0]},{comps[1]}", input);
+
             var l1 = input.Skip(i + 1).Where(x => x.Contains(comps[0])).Select(x=>x.Replace(comps[0], "").Trim('-')).ToList();
             var l2 = input.Skip(i + 1).Where(x => x.Contains(comps[1])).Select(x=>x.Replace(comps[1], "").Trim('-')).ToList();
             var intersect = l1.Intersect(l2);
@@ -25,10 +24,48 @@ class Day23 {
                 }
             }
         }
-        
-        
+
+        var sortLong = longest.Split(',').ToList();
+        sortLong.Sort();
+        longest = string.Join(',', sortLong);
+
+
         Console.ForegroundColor=ConsoleColor.Blue;
         Console.WriteLine($"result: {sumA}");
-        Console.WriteLine($"result B: {sumB}");
+        Console.WriteLine($"result B: {longest}");
+    }
+
+    static private string longest="";
+    static List<string>merk=new();
+
+    static void recurse(string current, IEnumerable<string>input) {
+        var cur = current.Split(',').ToList();
+        cur.Sort();
+        var merker = string.Join(',', cur);
+        if (merk.Contains(merker))
+            return;
+        merk.Add(merker);
+
+
+        List<string> meetAll=null;
+        foreach (var c in cur) {
+            var treffer = input.Where(x => x.Contains(c)).Select(x => x.Replace(c, "").Trim('-')).ToList();
+            if (meetAll == null) {
+                meetAll = treffer;
+            } else {
+                meetAll = meetAll.Intersect(treffer).ToList();
+                if (!meetAll.Any())
+                    return;
+            }
+        }
+        if (meetAll != null)
+            foreach (var m in meetAll) {
+                var next = current + "," + m;
+                if (next.Length > longest.Length) {
+                    longest = next;
+                    Console.WriteLine(longest);
+                }
+                recurse(next, input);
+            }
     }
 }
