bad4d87d99c3dd859134e141215a9266045fe4f2
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2024-12-23 23:07:42 +0100

    D22P2

--- Day22.cs
+++ Day22.cs
@@ -1,35 +1,43 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 internal class Day22 {
     internal static void doit() {
         Regex dayNoR = new(@"\d*$");
         var input = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType!.Name).Value)).Select(x=>long.Parse(x)).ToList();
         // input = """
-        //         """.Split('\n');
+        //         1
+        //         2
+        //         3
+        //         2024
+        //         """.Split('\n').Select(x=>long.Parse(x)).ToList();
 
-        SortedDictionary<int, Stack<int>> lastPrices = new();
-        SortedDictionary<string, List<int>> soldBananas = new();
+        SortedDictionary<int, Queue<int>> lastPrices = new();
+        SortedDictionary<int, long> lastSecrets = new();
+        SortedDictionary<string, SortedDictionary<int, int>> soldBananas = new();
 
         for (int i = 0; i < 2000; i++) {
             for (int j = 0; j < input.Count; j++) {
+                lastSecrets[j] = input[j];
                 var x = ((input[j] * 64) ^ input[j]) % 16777216;
                 x = ((long)Math.Floor((double)x/32) ^ x) % 16777216;
                 x = ((x * 2048) ^ x) % 16777216;
 
                 if (i == 0) {
-                    lastPrices[j] = new Stack<int>(4);
-                } else {
-                    var lastPrice = int.Parse($"{input[j]}".Last().ToString());
-                    var curPrice = int.Parse($"{x}".Last().ToString());
-                    lastPrices[j].Push(curPrice-lastPrice);
+                    lastPrices[j] = new Queue<int>(4);
                 }
+                var lastPrice = int.Parse(lastSecrets[j].ToString().Last().ToString());
+                var curPrice = int.Parse($"{x}".Last().ToString());
+                lastPrices[j].Enqueue(curPrice-lastPrice);
+                if (lastPrices[j].Count == 5) lastPrices[j].Dequeue();
 
                 if (i >= 4) {
                     string seq = String.Join(",",lastPrices[j]);
+                    if (seq == "-2,1,-1,3") Debugger.Break();
                     if (!soldBananas.ContainsKey(seq)) {
-                        soldBananas.Add(seq, new List<int>{j});
-                    } else if (!soldBananas[seq].Contains(j)) {
-                        soldBananas[seq].Add(j);
+                        soldBananas.Add(seq, new SortedDictionary<int, int>{{j, curPrice}});
+                    } else if (!soldBananas[seq].ContainsKey(j)) {
+                        soldBananas[seq].Add(j, curPrice);
                     }
                 }
 
@@ -39,6 +47,6 @@ internal class Day22 {
 
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine($"result: {input.Sum()}");
-        Console.WriteLine($"result B: {soldBananas.Values.Select(x => x.Count).Max()}");
+        Console.WriteLine($"result B: {soldBananas.Values.Select(x => x.Values.Sum()).Max()}");
     }
 }
